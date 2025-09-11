a448858399c84b85d1f2d9a987b10f1d8444ab3f
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2025-09-11 13:26:47 +0545

    Rename Try.tsx to SignupPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import {AuthCard} from "./pages/AuthCard"
 import SignupPage from "./pages/SignupPage"
-import Try from "./pages/Try"
 
 function App() {
 
   return (
     <>
-    <Try/>
+    <SignupPage/>
       <Routes>
         <Route path="/login" element={<AuthCard />} />
         <Route path="/signup" element={<SignupPage />} />
