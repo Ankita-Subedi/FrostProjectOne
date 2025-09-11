cf3523ddf7988269a027ce06e254a7a538ae95a2
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2025-09-11 13:22:06 +0545

    Delete unnecessary files

--- src/pages/Try.tsx
+++ src/pages/Try.tsx
@@ -3,14 +3,23 @@ import { AuthCard } from "./AuthCard";
 const Try = () => {
   return (
     <div className="relative h-screen w-screen flex overflow-hidden">
-      {/* Left Amber Section */}
+      {/* Left Section */}
       <div className="hidden lg:block w-full h-full relative z-0">
-        {/* Optional left-side image */}
+        {/* left-side image */}
         <img
           src="/mainBG.png"
           alt="Left"
           className="absolute top-1/2 left-0 -translate-y-1/2 object-cover z-10 h-screen w-screen"
         />
+        {/* Text overlay */}
+        <div className="absolute left-10 bottom-20 z-20 text-center text-white">
+          <h1 className="text-4xl font-bold">Securing Future Together</h1>
+          <p className="mt-4 text-lg w-2xl">
+            Join us in building a secure future together with the national loan
+            portal, designed to provide financial stability and peace of mind
+            for all members.
+          </p>
+        </div>
       </div>
 
       {/* Right Container */}
