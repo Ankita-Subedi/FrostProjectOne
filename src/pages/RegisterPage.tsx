a5af63c4f166cc72d39b3ba2ccdfd3cc75d96738
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2025-09-19 14:00:25 +0545

    Minor CSS changes

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -7,10 +7,10 @@ const RegisterPage = () => {
         <p className="rounded-full size-[93px] text-center flex justify-center items-center bg-secondary1 text-white">Logo</p>
       </div>
       <div>
-        <p className="text-primary1 text-[24px] font-bold text-center md:text-[36px]">Register your Tradesmart Account</p>
+        <p className="text-primary1 text-3xl font-bold text-center md:text-[36px]">Register your Tradesmart Account</p>
       </div>
       <div>
-        <p className="text-primary1 text-[12px] font-semibold text-center md:text-[18px]">Fill in your details below to create your NPPF account</p>
+        <p className="text-cyan-800 text-md font-semibold md:font-medium text-center md:text-[18px]">Fill in your details below to create your NPPF account</p>
       </div>
       <div className="">
         <Register/>
